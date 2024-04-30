e3a4d7365eaa57deb52f203c38ae3d8d4f095509
gcpearse <glen.c.pearse@gmail.com>
2024-04-30 17:32:45 +0100

    updates switch statement

--- C-Sharp-Fundamentals/Program.cs
+++ C-Sharp-Fundamentals/Program.cs
@@ -10,6 +10,7 @@ class Program
       {
         case "0": HelloWorld.Program.MainFn(); break;
         case "1": NumericDataTypes.Program.MainFn(); break;
+        case "2": TextDataTypes.Program.MainFn(); break;
         default: Console.WriteLine("Invalid argument."); break;
       }
     }
