28a630fab6538219beffb305a7f0dcf8ef0969c5
gcpearse <glen.c.pearse@gmail.com>
2024-04-30 17:12:47 +0100

    adds logic to allow user to run individual files from the command line

--- C-Sharp-Fundamentals/Program.cs
+++ C-Sharp-Fundamentals/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace Fundamentals;
+
+class Program
+{
+  public static void Main(string[] args)
+  {
+    if (args.Length == 1)
+    {
+      switch (args[0])
+      {
+        case "0": HelloWorld.Program.HelloWorld(); break;
+        default: Console.WriteLine("Invalid argument."); break;
+      }
+    }
+    else
+    {
+      Console.WriteLine("Please provide a single argument.");
+    }
+  }
+}
