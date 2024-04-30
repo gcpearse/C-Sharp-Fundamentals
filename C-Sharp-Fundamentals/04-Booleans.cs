f23f6b85b659503b216de27968206df0df5cf97a
gcpearse <glen.c.pearse@gmail.com>
2024-04-30 17:40:18 +0100

    adds booleans example

---
+++ C-Sharp-Fundamentals/04-Booleans.cs
@@ -0,0 +1,12 @@
+namespace Booleans;
+
+class Program
+{
+  public static void MainFn()
+  {
+    bool isActive = true;
+    Console.WriteLine(isActive);
+    isActive = false;
+    Console.WriteLine(isActive);
+  }
+}
