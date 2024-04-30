54b53354e6fe4af2cf841213cac18c33e7eb7a24
gcpearse <glen.c.pearse@gmail.com>
2024-04-30 22:35:16 +0100

    updates switch statement

--- C-Sharp-Fundamentals/Program.cs
+++ C-Sharp-Fundamentals/Program.cs
@@ -13,35 +13,43 @@ class Program
         case "2": TextDataTypes.Program.MainFn(); break;
         case "3": TypeConversion.Program.MainFn(); break;
         case "4": Booleans.Program.MainFn(); break;
+
         case "5": Operations.Program.MainFn(); break;
         case "6": Constants.Program.MainFn(); break;
         case "7": ConsoleIO.Program.MainFn(); break;
         case "8": IfStatements.Program.MainFn(); break;
         case "9": SwitchStatements.Program.MainFn(); break;
+
         case "10": ForLoops.Program.MainFn(); break;
         case "11": WhileLoops.Program.MainFn(); break;
         case "12": NumericFormatting.Program.MainFn(); break;
         case "13": TryParse.Program.MainFn(); break;
         case "14": TimesTable.Program.MainFn(); break;
+
         case "15": FizzBuzz.Program.MainFn(); break;
         case "16": StringFormatting.Program.MainFn(); break;
         case "17": Strings.Program.MainFn(); break;
         case "18": ReverseString.Program.MainFn(); break;
         case "19": VerifyPassword.Program.MainFn(); break;
+
         case "20": Arrays.Program.MainFn(); break;
         case "21": ArrayMethods.Program.MainFn(); break;
         case "22": Lists.Program.MainFn(); break;
         case "23": Dictionaries.Program.MainFn(); break;
         case "24": OddEvenSplit.Program.MainFn(); break;
+
         case "25": MultiplesArray.Program.MainFn(); break;
         case "26": Functions.Program.MainFn(); break;
         case "27": TriangleArea.Program.MainFn(); break;
         case "28": SumOfNumbers.Program.MainFn(); break;
         case "29": Exceptions.Program.MainFn(); break;
+
         case "30": CustomTryParse.Program.MainFn(); break;
         case "31": Structures.Program.MainFn(); break;
         case "32": Classes.Program.MainFn(); break;
         case "33": ClassMethods.Program.MainFn(); break;
+        case "34": ClassProperties.Program.MainFn(); break;
+
         default: Console.WriteLine("Invalid argument."); break;
       }
     }
