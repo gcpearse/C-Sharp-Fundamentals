75d2ea1a35a6fa2919202d77ef5bf4b22add1c3c
gcpearse <glen.c.pearse@gmail.com>
2024-04-30 17:32:32 +0100

    adds examples of text-based data types

---
+++ C-Sharp-Fundamentals/02-TextDataTypes.cs
@@ -0,0 +1,20 @@
+namespace TextDataTypes;
+
+class Program
+{
+  public static void MainFn()
+  {
+    string name = "James";
+    Console.WriteLine(name);
+
+    char letter = 'A';
+    Console.WriteLine(letter);
+  }
+}
+
+
+/*
+
+Store a string variable in "" and a char variable in ''.
+
+*/
